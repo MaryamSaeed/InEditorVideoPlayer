e3aaa2ae9e345ce3d910f9e47ce4b3ad140785fb
Maryam Kamel <Maryam.kamel@linkdev.com>
2019-12-11 00:20:53 +0200

    initial commit creating a window with custum menu item and shortcut

---
+++ VideoPlayer/Assets/Editor/VideoPlayerWindow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public class VideoPlayerWindow :EditorWindow
+{
+    [MenuItem("Video/VideoPlayerWindow _%p")]
+    public static void ShowVideoPlayerWindow()
+    {
+        var window = GetWindow<VideoPlayerWindow>("Unity Video Player");
+    }
+
+    private void OnEnable()
+    {
+        var root = rootVisualElement;
+    }
+
+    //private Button CreatButton(string buttontext,float width,float height)
+    //{
+    //    var newButton = new Button() { text = buttontext };
+    //    newButton.style.width = width;
+    //    newButton.style.height = height;
+    //    return newButton;
+    //}
+}
