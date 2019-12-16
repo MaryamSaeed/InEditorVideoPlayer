e81b670c768330b78e35a81c1c9999b98996d65c
Maryam Kamel <Maryam.kamel@linkdev.com>
2019-12-17 01:40:09 +0200

    adding new template to hold the video data and videoplayer component

--- VideoPlayer/Assets/Editor/Scripts/VideoPlayerWindow.cs
+++ VideoPlayer/Assets/Editor/Scripts/VideoPlayerWindow.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEngine.Video;
 
 public class VideoPlayerWindow :EditorWindow
 {
@@ -14,15 +15,26 @@ public class VideoPlayerWindow :EditorWindow
     {
         var root = rootVisualElement;
         root.styleSheets.Add(Resources.Load<StyleSheet>("VideoPlayerStyle"));
-        var buttonsVisualTree = Resources.Load<VisualTreeAsset>("VideoPlayerMain");
-        buttonsVisualTree.CloneTree(root);
-        var videoPlayerButtons = root.Query<Button>();
-        videoPlayerButtons.ForEach(SetupButton);
+        InitButtonContainer(root);
     }
     private void SetupButton(Button button)
     {
         var buttonIcon = button.Q(className: "videoplayer-button-icon");
         button.text = button.parent.name;
         button.clickable.clicked += () => Debug.Log("button " + button.parent.name);
+    }
+    private void InitButtonContainer(VisualElement root)
+    {
+        var buttonsVisualTree = Resources.Load<VisualTreeAsset>("VideoPlayerMain");
+        buttonsVisualTree.CloneTree(root);
+        var videoPlayerButtons = root.Query<Button>();
+        videoPlayerButtons.ForEach(SetupButton);
+    }
+    private void InitVideoArea(VisualElement root)
+    {
+        var videoVisualTree = new VisualTreeAsset();
+        videoVisualTree.CloneTree(root);
+        var videoPlayerComponent = new VideoPlayer();
+        videoPlayerComponent.url = "";
     }
 }
