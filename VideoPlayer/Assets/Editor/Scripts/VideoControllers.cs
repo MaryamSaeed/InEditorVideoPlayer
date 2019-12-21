e7eaa3de0469f692707590ef1bc251ed857dde59
Maryam Kamel <Maryam.kamel@linkdev.com>
2019-12-22 01:23:40 +0200

    adding video controller buttons to the scene

---
+++ VideoPlayer/Assets/Editor/Scripts/VideoControllers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoControllers
+{
+    private VideoPlayer videoPlayer;
+    public VideoControllers(VideoPlayer activeplayer)
+    {
+        videoPlayer = activeplayer;
+    }
+    public void PlayVideo()
+    {
+        videoPlayer.Play();
+    }
+    public void PauseVideo()
+    {
+        videoPlayer.Pause();
+    }
+    public void StopVideo()
+    {
+        videoPlayer.Stop();
+    }
+    public void FastForwardVideo()
+    {
+        if(videoPlayer.canStep)
+            videoPlayer.StepForward();
+    }
+   
+}
