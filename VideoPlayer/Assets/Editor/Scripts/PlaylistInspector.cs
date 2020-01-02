f03c1b6a1e2ce0e0953093f3d5e7dd9edbaa7afa
Maryam Kamel <MaryamSaeed@users.noreply.github.com>
2020-01-02 23:33:57 +0300

    finallu a working UI elemnts Inspector

--- VideoPlayer/Assets/Editor/Scripts/PlaylistInspector.cs
+++ VideoPlayer/Assets/Editor/Scripts/PlaylistInspector.cs
@@ -8,39 +8,26 @@ using UnityEditor.UIElements;
 [CustomEditor(typeof(PlaylistAsset))]
 public class PlaylistInspector : Editor
 {
-    private PlaylistAsset playlistAsset;
     private VisualElement root;
-    private VisualTreeAsset visualTree;
-    private SerializedProperty playlistTitle;
+    private PlaylistAsset targetAsset;
     void OnEnable()
     {
-        playlistAsset = (PlaylistAsset)target;
-        root = new VisualElement();
-        visualTree = Resources.Load<VisualTreeAsset>("UXMLs/PlaylistInspector");
+        targetAsset = (PlaylistAsset)target;
     }
 
     public override VisualElement CreateInspectorGUI()
     {
-        //Clear the visual element
+        root = new VisualElement();
         root.Clear();
-        ////Clone the visual tree into our Visual Element so it can be drawn
-        visualTree.CloneTree(root);
-        var prop = serializedObject.FindProperty("PlaylistTitle");
-        var propfiled = root.Q<PropertyField>("playlist-title-field");
-        propfiled.Bind(serializedObject);
-        Debug.Log("Title: " + playlistAsset.PlaylistTitle);
+        root.Add(new PropertyField(serializedObject.FindProperty("PlaylistTitle")));
+        root.Add(new Label("Drag videos directly to the list"));
+        root.Add(new PropertyField(serializedObject.FindProperty("VideoClipList")));
+        root.Bind(serializedObject);
+        targetAsset.PlaylistTitle = serializedObject.targetObject.name;
         serializedObject.ApplyModifiedProperties();
-        //Add A Callback For Each Button
-        //UQueryBuilder<VisualElement> builder = _RootElement.Query(classes: new string[] { "prefab-button" });
-        //builder.ForEach(AddButtonIcon);
-
         return root;
     }
-    //public override void OnInspectorGUI()
-    //{
-    //    serializedObject.Update();
-    //    EditorGUILayout.PropertyField(playlistTitle);
-    //    Debug.Log(playlistTitle.ToString());
-    //    serializedObject.ApplyModifiedProperties();
-    //}
+    private void OnChenge()
+    { }
+
 }
